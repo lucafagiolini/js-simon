90ebe3b3a380f03a2fc7cb7a2ce2eb853645011b
Luca <lucafagiolini@live.it>
2024-02-11 21:35:56 +0100

    i'm getting the hours, minutes and second difference in console log

--- js/script.js
+++ js/script.js
@@ -20,5 +20,14 @@ let countDown = setInterval(function () {
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
     console.log(days);
 
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    console.log(hours);
+
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    console.log(minutes);
+
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    console.log(seconds);
+
 }, 1000);
 
