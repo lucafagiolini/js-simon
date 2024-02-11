7f90e4b1cc7d1d5e6318c1c4759cace1aef66ec6
Luca <lucafagiolini@live.it>
2024-02-11 21:10:38 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// prendo la data da cui voglio partire inserisco la stringa manualmente,
+// in un secondo momento potrei inserire un imput per farlo inserire all'utente
+let countDownDate = new Date("Feb 12, 2024 09:30:00").getTime();
+console.log(countDownDate);
+
+
+
